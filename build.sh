fb651b4cde16f04c0c153094963884bc85902a16
Puttipong Wongrak <puttipong.wongrak@gmail.com>
2021-08-13 19:57:25 +0700

    create basic example deployment file

---
+++ build.sh
@@ -0,0 +1,11 @@
+# for development environment
+minikube start
+minikube start --vm=true
+minikube addons enable ingress
+
+# for public cloud environment
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+kubectl create namespace ingress-nginx
+helm install ingress-nginx ingress-nginx/ingress-nginx -n ingress-nginx -f overlays/std/values/ingress-values.yaml
